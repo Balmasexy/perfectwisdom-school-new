56b5e0518a27f3b734ab2d2acf8f2d95555cc6ea
balmasexy <www.alabamaabiodun@gmail.com>
2026-09-25 23:00:12 +0100

    add Perfect Wisdom School Android app

---
+++ apps/web/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.perfectwisdomschool.app',
+  appName: 'Perfect Wisdom School',
+  webDir: 'dist'
+};
+
+export default config;
